23d0231fbb8dfbeb102d4d09f0a05eef6c8a1b1f
tushar-811 <tushar.dhakal03@gmail.com>
2025-12-03 17:22:58 -0500

    Added PerformancesummaryVM

---
+++ MVC_Music/ViewModels/PerformanceSummaryVM.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Music.ViewModels
+	{
+	public class PerformanceSummaryVM
+		{
+		public int ID { get; set; }
+
+		
+		[Display(Name = "Musician")]
+		public string FormalName
+			{
+			get
+				{
+				return LastName + ", " + FirstName
+					+ (string.IsNullOrEmpty(MiddleName) ? "" :
+						(" " + (char?)MiddleName[0] + ".").ToUpper());
+				}
+			}
+
+		
+		public string FirstName { get; set; } = "";
+		public string? MiddleName { get; set; }
+		public string LastName { get; set; } = "";
+
+		
+		[Display(Name = "Number of Performances")]
+		public int? NumberOfPerformances { get; set; }
+
+		[Display(Name = "Average Fee Paid")]
+		[DataType(DataType.Currency)]
+		public double? AverageFeePaid { get; set; }
+
+		[Display(Name = "Highest Fee Paid")]
+		[DataType(DataType.Currency)]
+		public double? HighestFeePaid { get; set; }
+
+		[Display(Name = "Lowest Fee Paid")]
+		[DataType(DataType.Currency)]
+		public double? LowestFeePaid { get; set; }
+		}
+	}
